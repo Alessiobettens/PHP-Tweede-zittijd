e5d48997784903d6f60b668bf8cc5231d7768532
Alessiobettens <alessio.bettens@student.ehb.be>
2026-06-26 14:11:33 +0200

    Fix session handling order and protect index page with login redirect

--- public/index.php
+++ public/index.php
@@ -1,4 +1,11 @@
 <?php
+session_start();
+
 require __DIR__ . '/../config/db.php';
 
-echo "Database connected!";
+if (!isset($_SESSION['user_id'])) {
+    header("Location: login.php");
+    exit;
+}
+
+echo "Welkom! Je bent ingelogd.";
