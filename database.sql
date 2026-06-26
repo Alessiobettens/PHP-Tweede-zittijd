cb474611973b7aab991e63f7a640d6e4e6c62c12
Alessiobettens <alessio.bettens@student.ehb.be>
2026-06-26 18:56:11 +0200

    database-sql

--- database.sql
+++ database.sql
@@ -1,5 +1,7 @@
-CREATE TABLE users (
+CREATE TABLE lists (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    email VARCHAR(255),
-    password VARCHAR(255)
+    user_id INT,
+    title VARCHAR(255) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (user_id) REFERENCES users(id)
 );
